d6ec8a47433091d12132dfe0f329788445d394ca
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-24 20:03:20 +0900

    FEAT: item, address, delivery, deliveryStatus, member, order, orderitem, orderStatus 엔티티 구현

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,31 +10,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
 
-    @Test
-    @Transactional // test에서는 롤백 처리 함
-    @Rollback(false)
-    public void testMember() throws Exception {
-
-        // given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        // when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        // then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
-
-    }
 }
