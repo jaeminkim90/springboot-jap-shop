f4d4dbefa4a0b589c7889981a55097c72a64080b
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-28 13:04:30 +0900

    FEAT: ItemRepository, ItemService, Order 구현

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MemberServiceTest {
+
+    @Test
+    void 회원가입() throws Exception{
+
+    // given
+
+    // when
+
+    // then
+    }
+
+
+    @Test
+    void 중복_회원_예외() throws Exception{
+
+    // given
+
+    // when
+
+    // then
+    }
+
+}
